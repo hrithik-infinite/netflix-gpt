112e5977e4fc39e199cc6f4e0f639016f5c4e8b4
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-03-01 23:25:44 +0530

    api to nowPlayin movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { API_OPTIONS } from "../utils/Constants";
 const Browse = () => {
+  const getNowPlaying = async () => {
+    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1", API_OPTIONS)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
   return (
     <div>
       <Header />
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,10 @@
 export const BACKGROUND_IMAGE_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 export const NETFLIX_LOGO_PATH = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2Q1ODhhMzY1Y2NjYmIyOWNlNTg4MzY1NGM1Y2RlNCIsInN1YiI6IjY1ZTIxNDFlYTgwNjczMDE4NGFiOWEwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HHh-qdhdn0gt-jDExZbBddUF-gRGWjKx4cuMfFtFf5A",
+  },
+};
