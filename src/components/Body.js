77cf6b5792f31c010ae9652b41ff2e7cbe0e8611
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-03-01 20:48:57 +0530

    fix for issue where routes were not protected

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,9 @@
-import React, { useEffect } from "react";
-import { RouterProvider, createBrowserRouter, useNavigate } from "react-router-dom";
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
-import { auth } from "../utils/firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/userSlice";
+
 const Body = () => {
-  const dispatch = useDispatch();
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -18,17 +14,6 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const { uid, email, displayName, photoURL } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
-      } else {
-        dispatch(removeUser());
-      }
-    });
-  }, []);
 
   return (
     <div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NETFLIX_LOGO_PATH } from "../utils/Constants";
 import avatar from "../Assets/Netflix-avatar.png";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { addUser, removeUser } from "../utils/userSlice";
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
   const handleSignout = () => {
     signOut(auth)
@@ -18,6 +20,19 @@ const Header = () => {
         navigate("/error");
       });
   };
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/browse");
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
+      } else {
+        navigate("/");
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <div className="flex justify-between items-center absolute top-0 left-0 w-full z-10 bg-gradient-to-b from-black px-8 py-2 ">
       <img className="w-52" src={NETFLIX_LOGO_PATH} alt="logo" />
