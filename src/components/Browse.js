cc283ec2134a4348b070278a102748ffd1b6afd5
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-03-01 22:23:36 +0530

    addded unsubscribe for onauthchange hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Header from "./Header"
+import React from "react";
+import Header from "./Header";
 const Browse = () => {
   return (
     <div>
       <Header />
     </div>
-  )
-}
+  );
+};
 
-export default Browse
+export default Browse;
--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       });
   };
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         navigate("/browse");
         const { uid, email, displayName, photoURL } = user;
@@ -31,6 +31,7 @@ const Header = () => {
         dispatch(removeUser());
       }
     });
+    return () => unsubscribe();
   }, []);
 
   return (
