15e55c8054cfbcd1f79c7e535c2b5df0fdad62a7
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-03-16 20:04:20 +0530

    added truncation of overview

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -3,11 +3,20 @@ import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 const VideoTitle = ({ title, overview }) => {
+  const truncateOverview = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.substr(0, maxLength) + "...";
+    }
+    return text;
+  };
+
+  const truncatedOverview = truncateOverview(overview, 300);
+
   return (
     <div className="absolute mt-20 px-12 z-[0] left-0 top-[20vh] w-1/4">
-      <div className="relative bg-black bg-opacity-60 rounded-lg p-4 inline-flex flex-col ">
+      <div className="relative bg-black bg-opacity-60 rounded-lg p-4 inline-flex flex-col">
         <h1 className="text-4xl font-bold mb-2 text-white">{title}</h1>
-        <p className="text-lg mb-4 text-white">{overview}</p>
+        <p className="text-lg mb-4 text-white">{truncatedOverview}</p>
         <div className="flex space-x-4">
           <button className="bg-white text-black px-6 py-2 rounded-md shadow-lg hover:bg-red-700 transition duration-300">
             <PlayArrowIcon style={{ color: "black" }} /> Play
