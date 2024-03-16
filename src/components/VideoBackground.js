5f0458eb588ceffbb41a2490c5cef009a3b58b88
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-03-16 16:16:05 +0530

    fix for iframe controls

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -8,11 +8,11 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div className="h-full w-full absolute inset-0 pointer-events-none">
       <iframe
-        src={"https://www.youtube.com/embed/" + trailer?.key + "?autoplay=1&mute=1"}
+        src={"https://www.youtube.com/embed/" + trailer?.key + "?autoplay=1&mute=1&showinfo=0&controls=0"}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
-        className="h-full w-full pointer-events-auto"></iframe>
+        className="h-full w-full pointer-events-none"></iframe>
     </div>
   );
 };
