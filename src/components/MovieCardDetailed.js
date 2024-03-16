2c20711cc85a8b091186d562650d166943b01507
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-03-16 21:54:47 +0530

    added component for movie card hover

---
+++ src/components/MovieCardDetailed.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieCardDetailed = ({movie}) => {
+  return (
+    <div className='h-[500px] w-[500px] bg-white'>MovieCardDetailed</div>
+  )
+}
+
+export default MovieCardDetailed
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,17 +1,50 @@
-// MovieList.js
-import React from "react";
+import React, { useState } from "react";
 import MovieCard from "./MovieCard";
+import MovieCardDetailed from "./MovieCardDetailed";
 
 const MovieList = ({ title, movies }) => {
-  console.log("movies" , movies)
+  // const [hoveredMovie, setHoveredMovie] = useState(null);
+  // const [showDetail, setShowDetail] = useState(false);
+  // const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseEnter = (movie, event) => {
+    // setShowDetail(false);
+    const { top, left } = event.target.getBoundingClientRect();
+    const topLeft = { x: left, y: top };
+    // setPosition(topLeft);
+    // setTimeout(() => {
+    //   setHoveredMovie(movie);
+    //   setShowDetail(true);
+    // }, 200);
+  };
+
+  const handleMouseLeave = () => {
+    // setHoveredMovie(null);
+    // setShowDetail(false);
+  };
+
   return (
     <div className="my-8 relative">
       <h1 className="text-2xl font-semibold mb-4 text-white">{title}</h1>
-      <div className="flex overflow-x-auto no-scrollbar">
+      <div className="flex overflow-x-auto no-scrollbar relative">
         {movies &&
           movies.map((movie) => (
-            <div key={movie.id} className="flex-shrink-0 mr-4 cursor-pointer hover:scale-105" >
-              <MovieCard data={movie}  />
+            <div key={movie.id} className="flex-shrink-0 mr-4 cursor-pointer relative" onMouseEnter={(e) => handleMouseEnter(movie, e)} onMouseLeave={handleMouseLeave}>
+              <MovieCard data={movie} />
+              {/* <>{showDetail && hoveredMovie && hoveredMovie.id === movie.id && (
+                <div
+                  className="absolute"
+                  style={{
+                    top: position.y,
+                    left: position.x,
+                    zIndex: 1000,
+                  }}>
+                  <MovieCardDetailed movie={hoveredMovie} />
+                  
+                </div>
+                
+              )}
+              </> */}
             </div>
           ))}
       </div>
